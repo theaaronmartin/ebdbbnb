f9228c2ede41fbfb4ed69b1a52f9b0ba36bc0497
Aaron Martin <grav3serker@Aarons-MacBook-Pro.local>
2016-07-30 00:32:37 -0700

    making headway

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,27 @@
 class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+      if @place.save
+        redirect_to @place
+      else
+        render :new
+      end
+  end
+
+  private
+    def place_params
+      params.require(:place).permit(:location, :price, :dates_available)
+    end
 end
